feba9ef756509e87db1e79c3c0b07130dcaf4a66
sindhura2006@gmail.com <sindhura2006@gmail.com>
2022-01-26 13:23:37 +0000

    test

--- target/run/jaffle_shop/models/staging/stripe/stg_payments.sql
+++ target/run/jaffle_shop/models/staging/stripe/stg_payments.sql
@@ -2,18 +2,13 @@
   create or replace  view RAW.dbt_palleti.stg_payments 
   
    as (
-    with payment as
-(
-select
+    select
     id as payment_id,
     orderid as order_id,
     paymentmethod as payment_method,
     status,
+    -- amount is stored in cents, convert it to dollars
     amount / 100 as amount,
     created as created_at
-
 from raw.stripe.payment
-)
-
-select * from payment
   );
