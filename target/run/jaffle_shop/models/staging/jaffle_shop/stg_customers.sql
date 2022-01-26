73d85d314b33ee2df0d7ba7c8b66b389eb63b6fe
sindhura2006@gmail.com <sindhura2006@gmail.com>
2022-01-26 12:21:45 +0000

    test

--- target/run/jaffle_shop/models/staging/jaffle_shop/stg_customers.sql
+++ target/run/jaffle_shop/models/staging/jaffle_shop/stg_customers.sql
@@ -2,15 +2,9 @@
   create or replace  view RAW.dbt_palleti.stg_customers 
   
    as (
-    with customers as (
-    
     select 
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
-)
-
-select * from customers
+    id as customer_id,
+    first_name,
+    last_name
+from raw.jaffle_shop.customers
   );
